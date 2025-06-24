a0299cb6d6229a9a6a98d0e902c41677d2cd588f
Mercy Wacera <waceraaamercy@gmail.com>
2025-06-24 16:25:47 -0700

    Add navbar menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,25 @@
 import Link from "next/link"
+import Menu from "./Menu"
 
 const Navbar = () => {
     return (
-        <div className="h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
+        <div className="h-20 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative">
             {/* Modile */}
-            <Link href="/"> MAWA</Link>
+            <div className="h-full flex items-center justify-between">
+            <Link href="/"> 
+            <div className="text-2xl tracking-wide">MAWA</div> 
+            </Link>
+            <Menu/>
+            </div>
+
+            {/* BIGGER SCREEN */}
+            <div className="hidden md:flex items-center justify-between h-full">
+                {/*LEFT*/}
+                <div className=""></div>
+                {/*RIGHT */}
+                <div className=""></div>
+            </div>
+
         </div>
     )
 }
