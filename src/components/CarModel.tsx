d847c991dab38a4e2afbd0145a1f0fceb1b1c9d2
Mercy Wacera <waceraaamercy@gmail.com>
2025-06-24 18:58:05 -0700

    Add cart

---
+++ src/components/CarModel.tsx
@@ -0,0 +1,9 @@
+"use Client"
+
+const CartModel = () => {
+    return (
+        <div className="">CartModel</div>
+    )
+}
+
+export default CartModel
--- src/components/Navicons.tsx
+++ src/components/Navicons.tsx
@@ -1,8 +1,42 @@
 "use client"
 
+import Image from "next/image"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+import CartModel from "./CarModel"
+
 const NavIcons = () => {
+    const [isProfileOpen, setIsProfileOpen] = useState(false)
+    const [isCartOpen, setIsCartOpen] = useState(false)
+    const router = useRouter();
+
+    const isLoggedIn = false;
+
+    const handleProfile = () => {
+        if(!isLoggedIn) {
+            router.push(`/login`)
+        }
+        setIsProfileOpen((prev) => !prev);
+    };
+    
     return (
-        <div className="">NavIcons</div>
+        <div className=" flex items-center gap-4 xl:gap-6 relative">
+            <Image src="/profile.png" alt="" width={22} height={22} className="cursor-pointer" onClick={handleProfile}/>
+            {isProfileOpen && (
+                <div className="absolute p-4 rounded-md top-12 left-0 text-sm">
+                <Link href="/">Profile</Link>
+                <div className="mt-2 cursor-pointer">Logout</div>
+            </div>)}
+            <Image src="/notification.png" alt="" width={22} height={22} className="cursor-pointer"/>
+            <div className="relative cursor-pointer">
+            <Image src="/cart.png" alt="" width={22} height={22} className="cursor-pointer" onClick={(prev) => !prev}/>
+            <div className="absolute -top-4 -right-4 w-6 h-6 bg-lama rounded-full text-white text-sm flex items-center justify-center">2</div>
+            <div>
+            {isCartOpen && (<CartModel/>)}
+            </div>
+            </div>
+        </div>
     )
 }
 
