a8b7bc672838807b1b771ae555bf48c4d03153bd
Mercy Wacera <waceraaamercy@gmail.com>
2025-06-25 03:30:55 -0700

    Add Shopping Cart

--- src/components/Navicons.tsx
+++ src/components/Navicons.tsx
@@ -24,10 +24,10 @@ const NavIcons = () => {
         <div className=" flex items-center gap-4 xl:gap-6 relative">
             <Image src="/profile.png" alt="" width={22} height={22} className="cursor-pointer" onClick={handleProfile}/>
             {isProfileOpen && (
-                // <div className="absolute p-4 rounded-md top-12 left-0 text-sm shadow-[0_3px_10px">
-                <div className="absolute p-4 top-12 left-0 text-sm bg-white/10 backdrop-blur-md rounded-md shadow-[0_4px_16px_0_rgba(31,38,135,0.15)] z-20">
-                <Link href="/">Profile</Link>
-                <div className="mt-2 cursor-pointer">Logout</div>
+            // <div className="absolute p-4 rounded-md top-12 left-0 text-sm shadow-[0_3px_10px">
+            <div className="absolute p-4 top-8 left-0 text-sm bg-white/10 backdrop-blur-md rounded-md shadow-[0_4px_16px_0_rgba(31,38,135,0.15)] z-20">
+            <Link href="/">Profile</Link>
+            <div className="mt-2 cursor-pointer">Logout</div>
             </div>)}
             <Image src="/notification.png" alt="" width={22} height={22} className="cursor-pointer"/>
             <div className="relative cursor-pointer">
@@ -39,15 +39,16 @@ const NavIcons = () => {
             className="cursor-pointer"
             onClick={() => setIsCartOpen((prev) => !prev)}
             />
+           
+            </div>
             <div className="absolute -top-4 -right-4 w-6 h-6 bg-lama rounded-full text-white text-sm flex items-center justify-center">
             2
             </div>
             {isCartOpen && (
-            <div className="absolute top-8 right-0 z-50">
+            <div className="absolute">
             <CartModel />
             </div>
             )}
-            </div>
 
         </div>
     )
